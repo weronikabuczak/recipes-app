019dcff9af2f423456fd036304d6fd842c537c8d
weronikakurczyna <56171356+weronikakurczyna@users.noreply.github.com>
2022-07-13 12:10:05 +0200

    Add navbar

--- src/components/MainContainer/MainContainer.js
+++ src/components/MainContainer/MainContainer.js
@@ -1,7 +1,17 @@
+import {Fragment} from "react";
+import Header from "./Header/Header";
+import classes from './MainContainer.module.css';
+import NavigationBar from "./NavigationBar";
+
 const MainContainer = () => {
-return (
-    <div>jsdjfsdj</div>
-)
+    return (
+        <Fragment>
+            <div className={classes.main}>
+                <NavigationBar/>
+                <Header/>
+            </div>
+        </Fragment>
+    )
 }
 
 export default MainContainer;
---
+++ src/components/MainContainer/NavigationBar.js
@@ -0,0 +1,27 @@
+import {Link} from "react-router-dom";
+import classes from './NagivationBar.module.css';
+
+const NavigationBar = () => {
+    return (
+        <header className={classes.header}>
+            <Link to='/home'>
+                <div className={classes.title}>Recipes App</div>
+            </Link>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to='/home'>Home</Link>
+                    </li>
+                    <li>
+                        <Link to='/recipes'>Recipes</Link>
+                    </li>
+                    <li>
+                        <Link to='/shopping'>Shopping</Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    )
+};
+
+export default NavigationBar;
