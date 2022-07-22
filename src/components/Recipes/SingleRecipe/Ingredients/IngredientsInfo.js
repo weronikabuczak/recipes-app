731c841990f49a1a8d3b8c46bf38b5ccb48ec5b8
weronikakurczyna <56171356+weronikakurczyna@users.noreply.github.com>
2022-07-22 22:22:49 +0200

    First shape of adding ingredients to shopping list

--- src/components/Recipes/SingleRecipe/Ingredients/IngredientsInfo.js
+++ src/components/Recipes/SingleRecipe/Ingredients/IngredientsInfo.js
@@ -1,27 +1,53 @@
 import classes from './IngredientsInfo.module.css';
 import CustomButton from "../../../../UI/CustomButton";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import IngredientsForm from "./IngredientsForm";
 
 const IngredientsInfo = ({ingredients}) => {
+    const [selectedIngredients, setSelectedIngredients] = useState([]);
+
     const [showIngredientsForm, setShowIngredientsForm] = useState(false);
 
     const showIngredientsFormHandler = () => {
+        // dispatch action to send stuff to firebase
         setShowIngredientsForm(prevState => !prevState);
+    }
+
+    useEffect(() => {
+
+    }, [selectedIngredients]);
+
+    const selectIngredient = (name, amount, e) => {
+        if (e.target.checked) {
+            for (const ing of selectedIngredients) {
+                if (Object.values(ing).includes(name)) {
+                    return
+                }
+            }
+            setSelectedIngredients([{name, amount}, ...selectedIngredients]);
+            console.log(selectedIngredients);
+        }
     }
 
     return (
         <div className={classes.ingredients}>
-            <header className={classes.header}>Ingredients</header>
-            <CustomButton confirmation onClick={showIngredientsFormHandler}>Create shopping list</CustomButton>
+            <header className={classes.header}>
+                Ingredients
+                <CustomButton confirmation>Create shopping list</CustomButton>
+            </header>
             <ul className={classes.ingredients__list}>
                 {ingredients && Object.keys(ingredients).map((key, item) => (
-                    <li key={key}>
+                    <div>
+                        {/*<input type={"checkbox"} onClick={() => selectIngredient(key, ingredients[key])}/>*/}
+                        <input type={"checkbox"} onClick={(e) => selectIngredient(key, ingredients[key], e)}/>
+                        <li key={key}>
                             {key}
                             <span className={classes.quantity}>
                             {ingredients[key]}
                         </span>
                         </li>
+                    </div>
+
                 ))}
             </ul>
             {showIngredientsForm && <IngredientsForm setShowIngredientsForm={setShowIngredientsForm}/>}
