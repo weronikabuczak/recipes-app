4954a9e3ee862b2c5d3995e53f4f2883b5cf46c6
weronikakurczyna <56171356+weronikakurczyna@users.noreply.github.com>
2022-08-19 15:18:17 +0200

    New recipe form (ingredients)

--- src/components/Recipes/NewRecipe/IngredientsForm.js
+++ src/components/Recipes/NewRecipe/IngredientsForm.js
@@ -1,51 +1,34 @@
 import classes from "../../ShoppingList/NewProductForm.module.css";
 import CustomInput from "../../../UI/CustomInput";
-import {useContext, useRef, useState} from "react";
-import RecipeContext from "../../../store/recipe-context";
+import {useRef, useState} from "react";
 import CustomButton from "../../../UI/CustomButton";
 
-const IngredientsForm = () => {
-    const recipeContext = useContext(RecipeContext);
-    const [unit, setUnit] = useState();
-    const [ingredient, setIngredient] = useState({});
+const IngredientsForm = (props) => {
+
     const nameRef =  useRef();
     const amountRef = useRef();
 
-    const setUnitHandler = (e) => {
-        // props.setUnit(e.target.value);
-        setUnit(e.target.value);
-    }
-
     const addIngredientHandler = (e) => {
-        setIngredient({
-            name: nameRef.current.value,
-            amount: amountRef.current.value,
-            unit: unit
-        });
         e.preventDefault();
-        recipeContext.addIngredient(ingredient);
+        const name = nameRef.current.value;
+        const amount = amountRef.current.value;
+
+        props.setIngredients(ingredients => ({
+            ...ingredients,
+            [name]: amount
+        }));
+
     }
 
     return (
         <form onSubmit={addIngredientHandler}>
             <div className={classes['products__inputs']}>
                 <CustomInput type='text' required label='Name of the product' ref={nameRef}/>
-                <CustomInput type='number' required label='Amount' ref={amountRef}/>
-                <div className={classes['select__container']}>
-                    {/*todo custom select*/}
-                    <select className={classes.select} onChange={setUnitHandler}>
-                        <option value="">No unit</option>
-                        <option value="g">Gram</option>
-                        <option value="ml">Millilitre</option>
-                        <option value="tsp">Teaspoon</option>
-                        <option value="tbsp">Tablespoon</option>
-                    </select>
-                </div>
+                <CustomInput type='text' required label='Amount' ref={amountRef}/>
             </div>
             <CustomButton type='submit'>Submit</CustomButton>
         </form>
     )
-
 }
 
 export default IngredientsForm;
--- src/components/Recipes/NewRecipe/NewRecipe.js
+++ src/components/Recipes/NewRecipe/NewRecipe.js
@@ -1,48 +1,54 @@
 import classes from './NewRecipe.module.css';
 import CustomInput from "../../../UI/CustomInput";
 import IngredientsForm from "./IngredientsForm";
-import {useContext, useRef, useState} from "react";
-import RecipeContext, {RecipeContextProvider} from "../../../store/recipe-context";
+import {useRef, useState} from "react";
+import CustomButton from "../../../UI/CustomButton";
 
 const NewRecipe = () => {
     const nameRef = useRef();
     const amountRef = useRef();
-    const [unit, setUnit] = useState();
-    const recipeContext = useContext(RecipeContext);
-    console.log(recipeContext.ingredients);
+    const [ingredients, setIngredients] = useState({});
+    console.log(ingredients);
+
 
     return (
-        <RecipeContextProvider>
-            <section className={classes.card}>
-                <h1>Here you can add your own recipe</h1>
-                <form className={classes.form}>
-                    <div>
-                        <CustomInput label="Name" required type="text" maxLength='30'/>
-                    </div>
-                    <div>
-                        <CustomInput label="Short description" required type="text" maxLength='50'/>
-                    </div>
-                    <div className={classes['select__container']}>
-                        <select className={classes.select}>
-                            <option value="Easy">Easy</option>
-                            <option value="Medium">Medium</option>
-                            <option value="Hard">Hard</option>
-                        </select>
-                    </div>
-                    <div>
-                        <CustomInput label="Time" required type="text" maxLength='50'/>
-                    </div>
-                    <div>
-                        <CustomInput label="Preparation" required type="text" maxLength='400'/>
-                    </div>
-
-                    {/*    todo image   */}
-                </form>
+        <section className={classes.card}>
+            <h1>Here you can add your own recipe</h1>
+            <form className={classes.form}>
                 <div>
-                    <IngredientsForm nameRef={nameRef} amountRef={amountRef} setUnit={setUnit}/>
+                    <CustomInput label="Name" required type="text" maxLength='30'/>
                 </div>
-            </section>
-        </RecipeContextProvider>
+                <div>
+                    <CustomInput label="Short description" required type="text" maxLength='50'/>
+                </div>
+                <div className={classes['select__container']}>
+                    <select className={classes.select}>
+                        <option value="Easy">Easy</option>
+                        <option value="Medium">Medium</option>
+                        <option value="Hard">Hard</option>
+                    </select>
+                </div>
+                <div>
+                    <CustomInput label="Time" required type="text" maxLength='50'/>
+                </div>
+                <div>
+                    <CustomInput label="Preparation" required type="text" maxLength='400'/>
+                </div>
+
+                {/*    todo image   */}
+            </form>
+            <div>
+                <ul>
+                    {ingredients && Object.keys(ingredients).map((key) => (
+                        <li className={classes.list}>
+                            {key} - {ingredients[key]}
+                            <button className={classes['deleteIngredient__button']}>Delete</button>
+                        </li>
+                    ))}
+                </ul>
+                <IngredientsForm setIngredients={setIngredients}/>
+            </div>
+        </section>
 
     )
 }
