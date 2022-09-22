7b69017abbd57201ab42b1e4a7157720815639fb
weronikakurczyna <56171356+weronikakurczyna@users.noreply.github.com>
2022-09-22 12:38:41 +0200

    Rollback deploy changes

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,24 +1,10 @@
-import classes from './Profile.module.css';
-import authContext from "../../store/auth-context";
-import {useContext} from "react";
-
 const Profile = () => {
-    const auth = useContext(authContext);
-    const token = auth.token;
+    return(
+    <div>
 
-    const sendHandler = async () => {
-        const response = await fetch(`https://recipes-app-32684-default-rtdb.firebaseio.com/meals.json?auth=${token}`);
-
-        if (!response.ok) {
-            throw new Error('Cannot load content.');
-        }
-        const responseData = await response.json();
-        console.log(responseData);
-
-    }
-    return (
-        <button onClick={sendHandler}>sdfsd</button>
-    )
+        <h1>The page is under construction</h1>
+    </div>
+    );
 }
 
 export default Profile;
--- src/router-pages/ProfilePage.js
+++ src/router-pages/ProfilePage.js
@@ -1,4 +1,3 @@
-import Home from "../components/Home/Home";
 import Profile from "../components/Profile/Profile";
 
 const ProfilePage = () => {
